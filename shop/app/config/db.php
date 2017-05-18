afedde641773bdbb10dff0a7ecf43010ec409c2a
Jesús Federico Franco <j.francomedinilla@gmail.com>
2017-05-18 13:54:32 +0200

    Inicialize easy

---
+++ shop/app/config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=easy',
+    'username' => 'easy',
+    'password' => 'easy',
+    'charset' => 'utf8',
+    'tablePrefix' => '',
+    'enableSchemaCache' => true,
+];
